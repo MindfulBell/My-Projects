7ee51052daa5a5567da11cb36420a1360cac45ff
MindfulBell <tim.bell02@gmail.com>
2016-02-28 17:16:53 -0800

    es6 language updates

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,13 +3,13 @@
 $(document).ready(function(){
   
   // variables to hold two inputs, answer, what the operation is, and a flag to indicate when found an answer
-  var input1 = ''; 
-  var input2 = '';
-  var myOpp = '';
-  var foundAnswer = false;
+  let input1 = ''; 
+  let input2 = '';
+  let myOpp = '';
+  let foundAnswer = false;
   
   // function to evaluate the operation itself (do math)
-  var operation = function(num1, num2) {
+  let operation = (num1, num2) => {
     
    if (eval(num1 + myOpp + num2)%1 !==0) {
       return eval(num1 + myOpp +num2).toFixed(3).toString();
@@ -23,7 +23,7 @@ $(document).ready(function(){
   // If input1 is empty, make sure that pressing an operation does nothing as it would break the program sort of
   // else if input 1 and input 2 are full, treat the operation button as an equal sign also to display the answer as well as update input1 to equal the answer and input 2 to equal nothing (this is the same as the = below); this allows chaining together operations
   // else (i.e. just have input1 filled) this just inputs the myOpp value
-  $('.opp-button').click(function(){    
+  $('.opp-button').click(() => {    
     if (input1 === '') {
       input1 = '';
       input2 = '';
@@ -44,8 +44,8 @@ $(document).ready(function(){
   //other calc buttons
   //AC and CE just clear everything and set back to normal
   //numbers take number inputs, if oppDone is true it will start updating input2
-  $('.calcbutton-sm').click(function(){   
-    var pressedButton = $(this).html();    
+  $('.calcbutton-sm').click(() => {   
+    let pressedButton = $(this).html();    
     switch (pressedButton){
       case 'AC':
       case 'CE':
@@ -81,7 +81,6 @@ $(document).ready(function(){
         }
         break;
       case '.':        
-      console.log(input1, input2);
         if (myOpp === '') {
           if (foundAnswer) {            
             input1 = '';   
@@ -96,8 +95,7 @@ $(document).ready(function(){
         else if (input2.toString().indexOf('.') === -1) {
           input2 += $(this).html().toString();
           $('#display').html(input2);   
-        }      
-        
+        }              
         break;
       case '=':        
         $('#display').html(operation(input1, input2));        
